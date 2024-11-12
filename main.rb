911186f6de119edd0b500e1efd2fce4e97725db5
Aristóteles <aristotelesbr@gmail.com>
2024-11-12 07:20:25 -0300

    --no-message

--- main.rb
+++ main.rb
@@ -6,6 +6,18 @@ APP =
   Lennarb.new do |route|
     route.get '/' do |_req, res|
       res.status = 200
-      res.write '<h1>Hello, World!</h1>'
+      res.html '<h1>Hello, World!</h1>'
+    end
+
+    route.get '/up' do |_req, res|
+      res.status = 200
+      html = <<~HTML
+        <html>
+          <body style="background-color: green;">
+            <h1>OK</h1>
+          </body>
+        </html>
+      HTML
+      res.html html
     end
   end
